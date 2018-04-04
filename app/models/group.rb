e74cd594737fb35c35cf2a740d2a0df98c021827
Benson <bensonliu1990@hotmail.com>
2018-04-04 17:50:58 +0900

    added editing function

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,5 +1,7 @@
 class Group < ApplicationRecord
-  validates :name, presence: true,uniqueness: true
+
+  validates :name, presence: true, uniqueness: true
+  validates :name, presence: true, on: :update, allow_blank: false
 
   has_many :users, through: :members
   has_many :members
