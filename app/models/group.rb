4840d356d0741f6bcf03367d921081860ea380d5
Benson <bensonliu1990@hotmail.com>
2018-04-04 17:56:22 +0900

    removing unnecessary codes

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,8 +1,6 @@
 class Group < ApplicationRecord
-
   validates :name, presence: true, uniqueness: true
-  validates :name, presence: true, on: :update, allow_blank: false
-
+  
   has_many :users, through: :members
   has_many :members
 
